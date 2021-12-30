47049711119a006733061375b01f81a859659cb1
Deivison Leite <dlmacedo9@gmail.com>
2021-12-30 19:31:27 -0300

    Primeira questão

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void PrimeiraQuestao(int indice)
+{
+  int soma = 0;
+  int k = 0;
+
+  while (k < indice)
+  {
+    k = k + 1;
+    soma = soma + k;
+  }
+
+  Console.WriteLine($"Questão 1: o valor da variável soma é {soma}");
+}
+
+PrimeiraQuestao(13);
