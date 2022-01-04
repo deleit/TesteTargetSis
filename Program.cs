3a4e285dc24043545704e510c263687382564c76
Deivison Leite <dlmacedo9@gmail.com>
2022-01-04 17:28:19 -0300

    Terceira questão

--- Program.cs
+++ Program.cs
@@ -1,56 +1,155 @@
-﻿static void PrimeiraQuestao(int indice)
-{
-  int soma = 0;
-  int k = 0;
+﻿using Newtonsoft.Json.Linq;
 
-  while (k < indice)
+namespace TesteTargetSis;
+
+class Program
+{
+  static void Main(string[] args)
   {
-    k = k + 1;
-    soma = soma + k;
+    PrimeiraQuestao(13);
+    SegundaQuestao();
+    TerceiraQuestão();
   }
 
-  Console.WriteLine($"Questão 1: o valor da variável soma é {soma}");
-}
-
-static void SegundaQuestao()
-{
-  Console.WriteLine("Digite um número: ");
-  var entrada = Console.ReadLine();
-  int numero = 0;
-  if (entrada != null)
+  static void PrimeiraQuestao(int indice)
   {
-    numero = int.Parse(entrada);
-  }
+    int soma = 0;
+    int k = 0;
 
-  bool estaNaSequencia = false;
-
-  List<int> sequencia = new List<int>() { 0, 1 };
-
-  if (numero == 0 || numero == 1)
-    estaNaSequencia = true;
-  else
-  {
-    while (numero >= sequencia[^1])
+    while (k < indice)
     {
-      if (numero == sequencia[^1])
+      k = k + 1;
+      soma = soma + k;
+    }
+
+    Console.WriteLine($"Questão 1: o valor da variável soma é {soma}");
+    Console.WriteLine("----------");
+  }
+
+  static void SegundaQuestao()
+  {
+    Console.WriteLine("Questão 2: digite um número: ");
+    var entrada = Console.ReadLine();
+    int numero = 0;
+    if (entrada != null)
+    {
+      numero = int.Parse(entrada);
+    }
+
+    bool estaNaSequencia = false;
+
+    List<int> sequencia = new List<int>() { 0, 1 };
+
+    if (numero == 0 || numero == 1)
+      estaNaSequencia = true;
+    else
+    {
+      while (numero >= sequencia[^1])
       {
-        estaNaSequencia = true;
-        break;
-      }
-      else
-      {
-        int ultimoElemento = sequencia[sequencia.Count - 1];
-        int penultimoElemento = sequencia[sequencia.Count - 2];
-        sequencia.Add(ultimoElemento + penultimoElemento);
+        if (numero == sequencia[^1])
+        {
+          estaNaSequencia = true;
+          break;
+        }
+        else
+        {
+          int ultimoElemento = sequencia[sequencia.Count - 1];
+          int penultimoElemento = sequencia[sequencia.Count - 2];
+          sequencia.Add(ultimoElemento + penultimoElemento);
+        }
       }
     }
+
+    if (estaNaSequencia)
+      Console.WriteLine($"O número {numero} está na sequência de Fibonacci");
+    else
+      Console.WriteLine($"O número {numero} não está na sequência de Fibonnaci");
+
+    Console.WriteLine("----------");
   }
 
-  if (estaNaSequencia)
-    Console.WriteLine($"Questão 2: o número {numero} está na sequência de Fibonacci");
-  else
-    Console.WriteLine($"Questão 2: o número {numero} não está na sequência de Fibonnaci");
-}
+  static void TerceiraQuestão()
+  {
+    string arquivoDados = File.ReadAllText("C:\\TesteTargetSis\\dados.json");
+    JArray jsonArray = JArray.Parse(arquivoDados);
 
-PrimeiraQuestao(13);
-SegundaQuestao();
+    // Encontra o maior fautramento
+    double maiorFaturamento = 0;
+    int diaMaiorFaturamento = 0;
+
+    for (int i = 0; i < jsonArray.Count; i++)
+    {
+      JObject dados = JObject.Parse(jsonArray[i].ToString());
+      JToken? valorJToken = dados.GetValue("valor");
+      JToken? diaJToken = dados.GetValue("dia");
+      if (valorJToken != null && diaJToken != null)
+      {
+        double valor = valorJToken.ToObject<double>();
+        if (valor > maiorFaturamento)
+        {
+          maiorFaturamento = valor;
+          diaMaiorFaturamento = diaJToken.ToObject<int>();
+        }
+      }
+    }
+
+    // Encontra o menor faturamento
+    double menorFaturamento = maiorFaturamento;
+    int diaMenorFaturamento = 0;
+
+    for (int i = 0; i < jsonArray.Count; i++)
+    {
+      JObject dados = JObject.Parse(jsonArray[i].ToString());
+      JToken? valorJToken = dados.GetValue("valor");
+      JToken? diaJToken = dados.GetValue("dia");
+      if (valorJToken != null && diaJToken != null)
+      {
+        double valor = valorJToken.ToObject<double>();
+        if (valor > 0 && valor < menorFaturamento)
+        {
+          menorFaturamento = valor;
+          diaMenorFaturamento = diaJToken.ToObject<int>();
+        }
+      }
+    }
+
+    // Calcula a média mensal
+    double soma = 0;
+
+    for (int i = 0; i < jsonArray.Count; i++)
+    {
+      JObject dados = JObject.Parse(jsonArray[i].ToString());
+      JToken? valorJToken = dados.GetValue("valor");
+      if (valorJToken != null)
+      {
+        double valor = valorJToken.ToObject<double>();
+        soma += valor;
+      }
+    }
+
+    double mediaMensal = soma / jsonArray.Count;
+
+    // Encontra o número de dias no mês em que o valor de faturamento diário foi maior do que a média mensal
+    int diasMaioresQueMedia = 0;
+
+    for (int i = 0; i < jsonArray.Count; i++)
+    {
+      JObject dados = JObject.Parse(jsonArray[i].ToString());
+      JToken? valorJToken = dados.GetValue("valor");
+      if (valorJToken != null)
+      {
+        double valor = valorJToken.ToObject<double>();
+        if (valor > mediaMensal)
+        {
+          diasMaioresQueMedia++;
+        }
+      }
+    }
+
+    Console.WriteLine("Questão 3:");
+    Console.WriteLine($"O maior faturamento do mês foi {maiorFaturamento}, no dia {diaMaiorFaturamento}");
+    Console.WriteLine($"O menor faturamento do mês foi {menorFaturamento}, no dia {diaMenorFaturamento}");
+    Console.WriteLine($"Foram {diasMaioresQueMedia} dias com um faturamento maior do que a média mensal");
+    Console.WriteLine("-----------");
+  }
+}
